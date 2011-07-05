348082e4e134f3eee4ca467df2ae7cd9c680ff69
glenbot <glenbot@gmail.com>
2011-07-05 12:44:58 -0500

    Many updates to the schipul.harvestapp.com time entry search hack (which wasn't originally done). It now toggles the search to allow for compatbility with harvest javascript

--- schipul.harvestapp.com.js
+++ schipul.harvestapp.com.js
@@ -4,12 +4,22 @@ $('head').append('<link rel="stylesheet" href="' + jquery18css + '" type="text/c
 $('#add_day_entry_link').live('click', function() {
     var project_selector_wrap = $('#project_tasks_selector_cont');
     var project_selector = project_selector_wrap.find('#project_selector');
+    var project_label = project_selector_wrap.parent().find('label');
     var project_tasks = [];
     var project_tasks_lookup = {};
+    var html = '';
 
-    var sub_task_lookup = "timesheet.show_tasks_for_project(this.getAttribute('data-id'), '');";
-    var new_project_selector = '<input type="text" data-id="0" id="project_selector" name="project" value="" onblur="' + sub_task_lookup + '" />';
+    // setup the new elements
+    html = '<input id="project-search" style="display:none;" type="text" data-id="0" name="project-search" value="" />';
+    var project_search = $(html);
 
+    html = '<a id="project-search-tl" style="padding-left: 5px;" href="#" alt="Search for project" title="Search for project">Search</a>';
+    var search_toggle_link = $(html);
+
+    html = '<a id="project-search-cl" style="display: none; padding-left: 5px;" href="#" alt="Cancel search for project" title="Cancel search for project">Cancel</a>';
+    var search_cancel_link = $(html);   
+
+    // create a list and dictionary lookup for autocomplete
     project_selector.find('optgroup').each(function() {
        var label = $(this).attr('label');
        $(this).find('option').each(function(){
@@ -21,16 +31,55 @@ $('#add_day_entry_link').live('click', function() {
        });
     });
 
-    project_selector.hide();
-    project_selector_wrap.prepend(new_project_selector);
-    project_selector = project_selector_wrap.find('#project_selector');
-    project_selector.autocomplete({
+    // append the search link and search inputs
+    if (project_selector_wrap.find('#project-search-tl').length == 0) {
+      project_selector.css({'width': '80%'});
+      project_selector.after(search_toggle_link);
+      project_selector.after(search_cancel_link);
+      project_selector_wrap.prepend(project_search);
+    }
+
+    // set the toggle link to bring up the search selector and hide the drop down
+    $('#project-search-tl').live('click', function() {
+      // hide selector and show search
+      $(this).hide();
+      project_selector.hide();
+      project_search.css({'width': '80%'});
+      project_search.show();
+      project_search.focus();
+      search_cancel_link.show();
+      return false;
+    });
+
+    // set the toggle link to bring up the search selector and hide the drop down
+    $('#project-search-cl').live('click', function() {
+      // hide selector and show search
+      $(this).hide();
+      project_selector.show();
+      project_search.hide();
+      search_cancel_link.hide();
+      search_toggle_link.show();
+      return false;
+    });
+
+    // get the new project selector and enable autocomplete on it
+    project_search.autocomplete({
         delay: 100,
         source: project_tasks,
         select: function(event, ui) {
             var key = ui.item.value;
-            project_selector.attr('data-id', project_tasks_lookup[key]);
-            project_selector.trigger('blur');
+
+            // reset the label
+            project_label.html('Project / Task');
+            project_label.html(project_label.html() + ' > ' + key)
+            
+            // hide the search and select the correct project dropdown
+            project_selector.val(project_tasks_lookup[key]);
+            project_selector.trigger('change');
+            project_selector.show();
+            project_search.hide();
+            search_cancel_link.hide();
+            search_toggle_link.show();
         }
     });
 });
